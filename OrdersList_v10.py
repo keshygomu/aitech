471d65a8cf7c07af979bcb607dedeaf51a3c15d2
keshygomu <revistinha@gmail.com>
2025-09-14 11:30:46 +0900

    Update OrdersList_v10.py

--- OrdersList_v10.py
+++ OrdersList_v10.py
@@ -116,7 +116,11 @@ if "dados" in st.session_state:
         margin-top: -10px;
         margin-bottom: -10px;
     }
-
+    /* reduzir espaçamento vertical */
+    div[data-testid="stCheckbox"] {
+        margin-top: -10px;
+        margin-bottom: -10px;
+    }
     </style>
     """, unsafe_allow_html=True)
 
